868ab64b87f10b0394da22b663696bcc7816f832
João Victor Ipirajá <joaovictorpiraja@gmail.com>
2021-01-04 20:08:50 -0300

    First Commit

--- LearnSwiftUI/ContentView.swift
+++ LearnSwiftUI/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack{
+            Color(UIColor(red: 0.87, green: 0.60, blue: 0.40, alpha: 1.00)).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            Text("João Victor").font(Font.custom("Comfortaa", size: 40)).foregroundColor(.white)
+        }
+        
+            
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro Max"))
+            .previewDisplayName("iPhone 12 Pro Max")
+
     }
 }
