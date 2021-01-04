968e194eccbd44248c1eed7dd06b784b05205e9d
João Victor Ipirajá <joaovictorpiraja@gmail.com>
2021-01-04 13:16:02 -0300

    Initial Commit

---
+++ LearnSwiftUI/LearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIApp.swift
+//  LearnSwiftUI
+//
+//  Created by João Victor Ipirajá de Alencar on 04/01/21.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
