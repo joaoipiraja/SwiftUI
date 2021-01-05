636eb8eb431a016c2f928b101efc11d01343a9e6
João Victor Ipirajá <joaovictorpiraja@gmail.com>
2021-01-04 21:50:21 -0300

    Create Business Card View

--- LearnSwiftUI/ContentView.swift
+++ LearnSwiftUI/ContentView.swift
@@ -12,10 +12,28 @@ struct ContentView: View {
         
         ZStack{
             Color(UIColor(red: 0.87, green: 0.60, blue: 0.40, alpha: 1.00)).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            Text("João Victor").font(Font.custom("Comfortaa", size: 40)).foregroundColor(.white)
+            VStack{
+                Image("profilePicture")
+                    .resizable().aspectRatio(1,contentMode: .fit).frame(width: 150, height: 150).clipShape(Circle()).overlay(Circle().stroke(Color(UIColor(red: 0.36, green: 0.43, blue: 0.57, alpha: 1.00)),lineWidth: 5))
+                
+                
+                Text("João Victor").font(Font.custom("Comfortaa", size: 40)).foregroundColor(.white)
+                Text("IOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                
+                Divider()
+                
+                InfoView(text: "+55 (85) 999211825", imageName: "phone.fill")
+                InfoView(text: "joaovictorpiraja@gmail.com", imageName: "mail.fill")
+                
+                
+                
+            }
+            
         }
         
-            
+        
     }
 }
 
@@ -23,6 +41,21 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro Max"))
             .previewDisplayName("iPhone 12 Pro Max")
+        
+    }
+}
 
+struct InfoView: View {
+    let text:String
+    let imageName:String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 20).frame(height: 50).foregroundColor(.white).overlay(HStack{
+            
+            Image(systemName: imageName).foregroundColor(Color(UIColor(red: 0.56, green: 0.22, blue: 0.30, alpha: 1.00)))
+            
+            Text(text)
+        })
+        .padding(.all)
     }
 }
